622a8251036ef26d5f9904b1c17170dec122b74f
herbert <hd>
2017-11-12 22:49:12 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'flask_multiple_static_folders',
   packages = ['flask_multiple_static_folders'],
-  version = '1.0',
+  version = '1.1',
   description = 'This library allows Flask developers to use multiple static folders',
   author = 'Herbert Dawkins',
   author_email = 'DrDawkins@ClearScienceInc.com',
@@ -10,4 +10,7 @@ setup(
   download_url = 'https://github.com/sudouser2010/flask_multiple_static_folders/archive/1.0.tar.gz',
   keywords = ['flask', 'multiple', 'many', 'static', 'folders'],
   classifiers = [],
+  install_requires=[
+  'flask==0.12.2',
+  ],
 )
